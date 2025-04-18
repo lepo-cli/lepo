e7bf5adf06e7c42f55e50967edb1a5908236820f
Song Jeyeong <46372718+hooreique@users.noreply.github.com>
2025-04-18 11:19:01 +0900

    introduce saneStatus

---
+++ sane_status.ts
@@ -0,0 +1,123 @@
+import { join } from "@std/path/join";
+
+export type Exec = {
+  readonly command: string;
+  readonly output: string;
+};
+
+export type GitRepo = {
+  readonly path: string;
+  readonly status: Exec;
+  readonly files: Exec;
+};
+
+export type SaneStatus = {
+  readonly ["git-repositories"]: {
+    readonly ["git-repository"]: ReadonlyArray<GitRepo>;
+  };
+  readonly ["current-files"]: Exec;
+};
+
+const td = new TextDecoder();
+
+export const saneStatus = (cwd: string): SaneStatus => {
+  const repoPaths: string[] = [];
+
+  outer: for (const { name, isDirectory } of Deno.readDirSync(cwd)) {
+    if (!isDirectory) continue;
+
+    if (name === ".git") {
+      repoPaths.push(join(cwd, "."));
+      continue;
+    }
+
+    const child = join(cwd, name);
+
+    for (const { name: nm, isDirectory: isDir } of Deno.readDirSync(child)) {
+      if (!isDir) continue;
+
+      if (nm === ".git") {
+        repoPaths.push(child);
+        continue outer;
+      }
+    }
+  }
+
+  const gitRepos = repoPaths
+    .map((path): Readonly<[string, Exec]> | false => {
+      const { success, stdout } = new Deno.Command("git", {
+        args: [
+          "-C",
+          path,
+          "--git-dir",
+          ".git",
+          "-c",
+          "core.quotepath=false",
+          "status",
+        ],
+      }).outputSync();
+
+      if (!success) return false;
+
+      return [
+        path,
+        {
+          command:
+            `git -C "${path}" --git-dir .git -c core.quotepath=false status`,
+          output: td.decode(stdout),
+        },
+      ];
+    })
+    .filter((item): item is Readonly<[string, Exec]> => item as boolean)
+    .map(([path, status]): GitRepo | false => {
+      const { success, stdout } = new Deno.Command("git", {
+        args: [
+          "-C",
+          path,
+          "--git-dir",
+          ".git",
+          "-c",
+          "core.quotepath=false",
+          "ls-files",
+        ],
+      }).outputSync();
+
+      if (!success) return false;
+
+      return {
+        path,
+        status,
+        files: {
+          command:
+            `git -C "${path}" --git-dir .git -c core.quotepath=false ls-files`,
+          output: td.decode(stdout),
+        },
+      };
+    })
+    .filter((item): item is GitRepo => item as boolean)
+    .toSorted(({ path: a }, { path: b }) => a.localeCompare(b, "en-US"));
+
+  const { success, stdout } = new Deno.Command("fd", {
+    args: [
+      "--unrestricted",
+      "--exclude",
+      "**/.git/*",
+      "--max-depth",
+      "2",
+      "--absolute-path",
+      ".*",
+      cwd,
+    ],
+  }).outputSync();
+
+  if (!success) throw new Error();
+
+  return {
+    ["git-repositories"]: { ["git-repository"]: gitRepos },
+    ["current-files"]: {
+      command:
+        `fd --unrestricted --exclude '**/.git/*' --max-depth 2 --absolute-path '.*' "${cwd}"`,
+      output: td.decode(stdout),
+    },
+  };
+};
---
+++ sane_status_test.ts
@@ -0,0 +1,91 @@
+import { assert, assertEquals, assertFalse } from "jsr:@std/assert@1.0.12";
+
+import { saneStatus } from "./sane_status.ts";
+
+import { join } from "@std/path/join";
+
+const BATCH = `
+echo 'repo_1
+repo_2' > .gitignore
+echo a > a.txt
+
+mkdir -p repo_1 sub/repo_2 not_a_repo
+
+echo b > repo_1/b.txt
+echo c > sub/c.txt
+echo d > sub/repo_2/d.txt
+echo e > not_a_repo/e.txt
+
+git init --initial-branch repo_0
+git add .
+git commit --message init
+
+git -C repo_1 init --initial-branch repo_1
+git -C repo_1 add .
+git -C repo_1 commit --message init
+
+git -C sub/repo_2 init --initial-branch repo_2
+git -C sub/repo_2 add .
+git -C sub/repo_2 commit --message init
+`;
+
+Deno.test({
+  name: "saneStatus",
+  permissions: {
+    read: true,
+    write: true,
+    run: true,
+  },
+  fn: () => {
+    const testdir = join(
+      import.meta.dirname as string,
+      "testspace",
+      "sane_status",
+    );
+
+    Deno.mkdirSync(testdir, { recursive: true });
+    Deno.removeSync(testdir, { recursive: true });
+    Deno.mkdirSync(testdir, { recursive: true });
+
+    try {
+      Deno.chdir(testdir);
+      assertEquals(Deno.cwd(), testdir);
+
+      {
+        assert(
+          new Deno.Command("bash", { args: ["-c", BATCH] })
+            .outputSync().success,
+        );
+      }
+
+      const {
+        ["git-repositories"]: { ["git-repository"]: [r0, r1, na] },
+        ["current-files"]: { output: fdOutput },
+      } = saneStatus(testdir);
+
+      assertFalse(na);
+
+      assertEquals(r0.path, testdir);
+      assertEquals(
+        r0.status.output,
+        "On branch repo_0\nnothing to commit, working tree clean\n",
+      );
+      assertEquals(
+        r0.files.output,
+        ".gitignore\na.txt\nnot_a_repo/e.txt\nsub/c.txt\n",
+      );
+
+      assertEquals(r1.path, join(testdir, "repo_1"));
+      assertEquals(
+        r1.status.output,
+        "On branch repo_1\nnothing to commit, working tree clean\n",
+      );
+      assertEquals(r1.files.output, "b.txt\n");
+
+      assert(fdOutput.trim().split("\n").length === 11);
+    } finally {
+      Deno.mkdirSync(testdir, { recursive: true });
+      Deno.removeSync(testdir, { recursive: true });
+    }
+  },
+});
