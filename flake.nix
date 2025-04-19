f4f3bc9488c29a9cea6468caea163758955eaa8f
Song Jeyeong <46372718+hooreique@users.noreply.github.com>
2025-04-19 16:05:07 +0900

    wrapProgram

--- flake.nix
+++ flake.nix
@@ -12,39 +12,26 @@
       in {
         defaultPackage = pkgs.stdenv.mkDerivation {
           pname    = "lepo";
-          version  = "0.1.0";
+          version  = "0.0.0";
           src      = ./.;
           LC_ALL = "en_US.UTF-8";
-          buildInputs = [
-            pkgs.fd
-            pkgs.ripgrep
-            pkgs.perl
-            pkgs.jq
-            pkgs.git
-            pkgs.openssh
-            pkgs.curl
-            pkgs.elinks
-          ];
-          nativeBuildInputs = [
-            pkgs.deno
-            pkgs.fd
-            pkgs.git
-          ];
+          nativeBuildInputs = [ pkgs.makeWrapper pkgs.deno pkgs.fd pkgs.git ];
           buildPhase = "DENO_DIR=$TMPDIR deno task build";
-          installPhase = "mkdir -p $out/bin && cp lepo $out/bin/lepo";
+          installPhase = ''
+            mkdir -p $out/bin
+            cp lepo $out/bin/lepo
+            wrapProgram $out/bin/lepo \
+              --prefix-each PATH : "${pkgs.lib.makeBinPath [
+                pkgs.fd  pkgs.ripgrep pkgs.perl pkgs.jq
+                pkgs.git pkgs.openssh pkgs.curl pkgs.elinks
+              ]}"
+          '';
         };
         devShells.default = pkgs.mkShell {
           LC_ALL = "en_US.UTF-8";
           buildInputs = [
-            pkgs.deno
-            pkgs.fd
-            pkgs.ripgrep
-            pkgs.perl
-            pkgs.jq
-            pkgs.git
-            pkgs.openssh
-            pkgs.curl
-            pkgs.elinks
+            pkgs.deno pkgs.fd      pkgs.ripgrep pkgs.perl   pkgs.jq
+            pkgs.git  pkgs.openssh pkgs.curl    pkgs.elinks
           ];
         };
       });
