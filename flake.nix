b2b973279442ea428d0124b780fabc79f13253ae
Song Jeyeong <46372718+hooreique@users.noreply.github.com>
2025-04-19 13:28:32 +0900

    add defaultPackage in flake

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Llm tool for rEPOsitory";
+  description = "Lepo Flakes";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -10,6 +10,33 @@
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
       in {
+        defaultPackage = pkgs.stdenv.mkDerivation {
+          pname    = "lepo";
+          version  = "0.1.0";
+          src      = ./.;
+
+          # Runtime deps
+          buildInputs = [
+            pkgs.fd
+            pkgs.ripgrep
+            pkgs.perl
+            pkgs.jq
+            pkgs.git
+            pkgs.openssh
+            pkgs.curl
+            pkgs.elinks
+          ];
+
+          # Build-time deps
+          nativeBuildInputs = [
+            pkgs.deno
+            pkgs.fd
+            pkgs.git
+          ];
+
+          buildPhase = "DENO_DIR=$TMPDIR deno task build";
+          installPhase = "mkdir -p $out/bin && cp lepo $out/bin/lepo";
+        };
         devShells.default = pkgs.mkShell {
           buildInputs = [
             pkgs.deno
