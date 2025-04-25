9b3d3118cf5ec2406bb7f72565df661ecb0e1bb2
Song Jeyeong <46372718+hooreique@users.noreply.github.com>
2025-04-25 17:04:08 +0900

    add dontStrip = true

--- flake.nix
+++ flake.nix
@@ -7,32 +7,36 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        defaultPackage = pkgs.stdenv.mkDerivation {
-          # Works with `sanbox = relaxed` in nix.conf on Linux
-          __noChroot = true;
-          pname = "lepo";
-          version = "0.0.0";
-          src = ./.;
-          nativeBuildInputs = [ pkgs.makeWrapper pkgs.deno pkgs.fd pkgs.git ];
-          buildPhase = "DENO_DIR=$TMPDIR deno task build";
-          installPhase = ''
-            mkdir -p $out/bin
-            cp lepo $out/bin/lepo
-            wrapProgram $out/bin/lepo \
-              --prefix-each PATH : "${pkgs.lib.makeBinPath [
-                pkgs.fd  pkgs.ripgrep pkgs.perl pkgs.jq
-                pkgs.git pkgs.openssh pkgs.curl pkgs.elinks
-              ]}"
-          '';
-        };
-        devShells.default = pkgs.mkShell {
-          buildInputs = [
-            pkgs.deno pkgs.fd      pkgs.ripgrep pkgs.perl   pkgs.jq
-            pkgs.git  pkgs.openssh pkgs.curl    pkgs.elinks
-          ];
-        };
-      });
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      packages.default = pkgs.stdenv.mkDerivation {
+        # Set sandbox = relaxed in ~/.config/nix/nix.conf on Linux
+        __noChroot = true;
+        pname = "lepo";
+        version = "0.0.0";
+        src = ./.;
+        nativeBuildInputs = [ pkgs.makeWrapper pkgs.deno pkgs.fd pkgs.git ];
+        buildPhase = ''
+          export DENO_DIR=$TMPDIR/.deno
+          deno task test --frozen && deno task compile --frozen
+        '';
+        installPhase = ''
+          mkdir -p $out/bin
+          cp lepo $out/bin/lepo
+          wrapProgram $out/bin/lepo \
+            --prefix-each PATH : "${pkgs.lib.makeBinPath [
+              pkgs.fd  pkgs.ripgrep pkgs.perl pkgs.jq
+              pkgs.git pkgs.openssh pkgs.curl pkgs.elinks
+            ]}"
+        '';
+        dontStrip = true;
+      };
+      devShells.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.deno pkgs.fd      pkgs.ripgrep pkgs.perl   pkgs.jq
+          pkgs.git  pkgs.openssh pkgs.curl    pkgs.elinks
+        ];
+      };
+    });
 }
